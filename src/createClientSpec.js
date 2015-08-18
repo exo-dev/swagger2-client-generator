4ffb9bd96a3bf2369fe603f20e625c8cab62cfdd
Gavrilov Seva <gavrilovseva@gmail.com>
2015-08-18 16:13:47 -0300

    Added some test for createClient.

--- src/createClientSpec.js
+++ src/createClientSpec.js
@@ -36,7 +36,16 @@ describe('create client', function(){
           type: 'apiKey',
           in: 'query',
           name: 'token'
+        },
+        basicAuth: {
+          type: 'basic',
+          in: 'query'
         }
+      },
+      definitions: {
+        Model: {properties: {
+          id: {type: 'string'}
+        }}
       }
     }
   });
@@ -46,9 +55,17 @@ describe('create client', function(){
     expect(client.resource).toBeDefined();
   });
 
-  it('uses the operation nickname for the operation name', function(){
+  it('uses the operation id for the operation name', function(){
     var client = createClient(schema, requestHandler);
     expect(client.resource.doIt).toBeDefined();
+  });
+
+  it('uses globally defined type if $ref is used', function() {
+    schema.paths['/resource/all-of-it'].get.parameters[0] = {
+      schema: {$ref: '#/definitions/Model'},
+    };
+    var client = createClient(schema, requestHandler);
+    expect(client.resource.doIt.operation.parameters[0].type).toBe('Model');
   });
 
   it('has the ability to set auth at many levels', function(){
@@ -69,15 +86,25 @@ describe('create client', function(){
     expect(client.authorization).toBeDefined();
   });
 
-  it('provides the most specific auth data passed in to it (resource-level)', function(){
+  it('provides the most specific auth data passed in to it (api-level)', function(){
     schema.paths['/resource/all-of-it'].get.security = [{apiKey: {}}];
     var client = createClient(schema, requestHandler);
 
     client.auth('api-level-auth');
-    client.resource.auth('resource-level-auth');
     client.resource.doIt('1');
     expect(requestHandler.calls.mostRecent().args[1].url)
-      .toBe('http://example.com/api/resource/all-of-it?token=resource-level-auth&queryParam=1');
+      .toBe('http://example.com/api/resource/all-of-it?token=api-level-auth&queryParam=1');
+  });
+
+  it('provides the most specific auth data passed in to it (model-level)', function(){
+    schema.paths['/resource/all-of-it'].get.security = [{apiKey: {}}];
+    var client = createClient(schema, requestHandler);
+
+    client.auth('api-level-auth');
+    client.resource.auth('model-level-auth');
+    client.resource.doIt('1');
+    expect(requestHandler.calls.mostRecent().args[1].url)
+      .toBe('http://example.com/api/resource/all-of-it?token=model-level-auth&queryParam=1');
   });
 
   it('provides the most specific auth data passed in to it (op-level)', function(){
@@ -91,4 +118,16 @@ describe('create client', function(){
     expect(requestHandler.calls.mostRecent().args[1].url)
       .toBe('http://example.com/api/resource/all-of-it?token=operation-level-auth&queryParam=1');
   });
+
+  it('provides username and password for basic authentication', function(){
+    schema.paths['/resource/all-of-it'].get.security = [{basicAuth: {}}];
+    var client = createClient(schema, requestHandler);
+
+    client.auth('john_doe', 'secret');
+    client.resource.doIt('1');
+    expect(requestHandler.calls.mostRecent().args[1].url)
+      .toBe('http://john_doe:secret@example.com/api/resource/all-of-it?queryParam=1');
+  });
+
+
 });
